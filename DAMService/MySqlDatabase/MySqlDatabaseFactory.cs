6d6f594046f1c1f3d915a87e8cbc454ede52ba79
NightFIsh <nightfish.net@gmail.com>
2022-01-16 20:51:07 +1300

    Expose mysql connection factory.

--- DAMService/MySqlDatabase/MySqlDatabaseFactory.cs
+++ DAMService/MySqlDatabase/MySqlDatabaseFactory.cs
@@ -11,7 +11,7 @@ namespace MySqlDatabase
     public class MySqlDatabaseFactory : IDatabaseFactory
     {
         private readonly ILogger _logger;
-        private readonly MySqlConnectionFactory _connectionFactory;
+        public MySqlConnectionFactory ConnectionFactory { get; private set; }
         
         public MySqlDatabaseFactory(ILoggerFactory loggerFactory, IOptions<MySqlOptions> options)
         {
@@ -24,25 +24,25 @@ namespace MySqlDatabase
             MySqlOptions config = options?.Value ?? throw new ArgumentNullException(nameof(options));
             
             // Initialize MySql
-            _connectionFactory = new MySqlConnectionFactory(config.ConnectionString);
+            ConnectionFactory = new MySqlConnectionFactory(config.ConnectionString);
             Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
         }
         
         public IRepositoryDatabase GetRepositoryDatabase()
         {
-            using MySqlConnection con = _connectionFactory.CreateConnection();
+            using MySqlConnection con = ConnectionFactory.CreateConnection();
             return new MySqlRepositoryDatabase(con);
         }
 
         public IAssetDatabase GetAssetDatabase(int repositoryId)
         {
-            using MySqlConnection con = _connectionFactory.CreateConnection();
+            using MySqlConnection con = ConnectionFactory.CreateConnection();
             return new MySqlAssetDatabase(con, repositoryId);
         }
         
         public IAssetFileDatabase GetAssetFileDatabase(int repositoryId, int assetId) // Repository ID is not used since the mysql schema uses globally unique IDs
         {
-            using MySqlConnection con = _connectionFactory.CreateConnection();
+            using MySqlConnection con = ConnectionFactory.CreateConnection();
             return new MySqlAssetFileDatabase(con, assetId);
         }
     }
